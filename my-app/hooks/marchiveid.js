2636c077ffaedbde70b2f317ccea814e0e85f5a5
konan1202 <machida0507@icloud.com>
2022-09-21 20:11:37 +0900

    途中

--- my-app/hooks/marchiveid.js
+++ my-app/hooks/marchiveid.js
@@ -1,16 +1,15 @@
 import axios from "axios"
 import { useState } from "react"
 
+function usemarchiveid() {
+  const [res, setres] = useState();
+  const marchiveidlist = async () => {
+    // GETリクエスト（通信）
+    const url = await axios.get("http://localhost:8000/api/v1/mock/archive/{id}/");
+    setres(url);
+  };
 
-const usemarchiveid = ()=>{
-    const [res,setres] = useState()
-    const marchiveidlist = async () => {
-        // GETリクエスト（通信）
-      const url = await axios.get("http://localhost:8000/api/v1/mock/archive/{id}/")
-      setres(url)
-    }
-
-    return {marchiveidlist,res}
+  return { marchiveidlist, res };
 }
 
 
---
+++ my-app/pages/1_mpost_id_re.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import React from "react";
+
+
+const mpost_id_re = ({ data, error }) => {
+    if (error) {
+        return <div>An error occured: {error.message}</div>
+    }
+    const id = data.id
+    const thumbsup = data.thumbsup
+    const heart = data.heart
+    const smile = data.smile
+    const astonished = data.astonished
+    return (
+        <div>
+            <dl>
+                <dt>Id</dt>
+                <dd>{id}</dd>
+                <dt>Thumbsup</dt>
+                <dd>{thumbsup}</dd>
+                <dt>Heart</dt>
+                <dd>{heart}</dd>
+                <dt>Smile</dt>
+                <dd>{smile}</dd>
+                <dt>Astonished</dt>
+                <dd>{astonished}</dd>
+            </dl>
+        </div>
+    )
+}
+
+mpost_id_re.getInitialProps = async ctx => {
+    try {
+        const response = await axios.get("http://localhost:8000/api/v1/mock/post/2/reaction/?e_mail=abc@email.com")
+        const data = response.data
+        return { data }
+    } catch (error) {
+        return { error }
+    }
+}
+
+export default mpost_id_re
